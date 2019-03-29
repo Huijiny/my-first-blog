3827ea69f4567a3ffe72b4e782495fde707c182a
Heejin Ryu <jiin20803@gmail.com>
2019-03-29 19:48:53 +0900

    My Django Girls App,

---
+++ python_intro.py
@@ -0,0 +1,14 @@
+girls = ["진석","희진","의석"]
+
+def hi(name):
+    print("Hello! "+name + "!")
+
+for girl in girls:
+    hi(girl)
+    print("Next girl")
+
+for i in [1,2,3,4,5]:
+    print(i**2)
+
+for i in range(1,6):
+    print(i**2)
